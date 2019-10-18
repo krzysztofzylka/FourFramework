8c1eb1cb3af8103ada2478254810607009f36197
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2019-10-18 08:51:27 +0200

    Update main.php

--- module/account/main.php
+++ module/account/main.php
@@ -7,7 +7,7 @@ return new class(){ //create main class
 		if(core::$library->database->isConnect === false)// if not connect
 			return core::setError(1, 'no connection to the database'); //return error 1
 	}
-	public function _install(){ //install module to database
+	public function _install() : bool { //install module to database
 		$sql = "CREATE TABLE user (
 			id INT(11) AUTO_INCREMENT PRIMARY KEY,
 			login VARCHAR(30) NOT NULL,
@@ -21,7 +21,7 @@ return new class(){ //create main class
 			return core::setError(1, 'error create table', $error->getMessage()); //return error 1
 		}
 	}
-	public function createUser(string $login, string $password, string $email){ //add user to database
+	public function createUser(string $login, string $password, string $email) : bool { //add user to database
 		$password = core::$library->crypt->hash(htmlspecialchars($password), $this->hashAlghoritm); //crypt password
 		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) //check email
 			return core::setError(1, 'Invalid email format'); //return error 1
@@ -32,7 +32,7 @@ return new class(){ //create main class
 		core::$library->database->conn->exec($exec); //exec script
 		return true; //return success
 	}
-	public function loginUser(string $login, string $password){ //login user
+	public function loginUser(string $login, string $password) : bool { //login user
 		$login = htmlspecialchars($login); //protect login
 		$password = htmlspecialchars($password); //protect password
 		$user = core::$library->database->conn->query('SELECT count(id) as count, id, password FROM user WHERE login="'.$login.'"')->fetch(PDO::FETCH_ASSOC); //get user from database
@@ -44,18 +44,19 @@ return new class(){ //create main class
 		$_SESSION[$this->sessionName] = (int)$user['id']; //set session
 		return true; //return success
 	}
-	public function checkUser(){ //check user login
+	public function checkUser() : bool { //check user login
 		if(!isset($_SESSION[$this->sessionName])) //check is isset session
 			return false; //return false
 		elseif(!is_int($_SESSION[$this->sessionName])) //check int
 			return false; //return false
 		return true; //return success
 	}
-	public function logoutUser(){ //logout
+	public function logoutUser() : bool{ //logout
 		if($this->checkUser() === false) //check user
 			return core::setError(1, 'the user is not logged in'); //return error 1
 		unset($_SESSION[$this->sessionName]); //delete session
 		$this->userData = null; //delete userData
+		return true;
 	}
 	public function userGetData(){ //get user data
 		$userID = (int)htmlspecialchars($_SESSION[$this->sessionName]); //protect user ID
