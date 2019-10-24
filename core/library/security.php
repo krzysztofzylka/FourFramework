a20c2e0f3f151bbab392cd4820bc2b37bae15d25
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2019-10-24 15:32:02 +0200

    1.0a
    Dodano czywszczenie błędu w funkcji isXMLHttpRequest

--- core/library/security.php
+++ core/library/security.php
@@ -1,26 +1,27 @@
 <?php
-return $this->security = new class(){ //create security library
-	public $version = '1.0'; //version
-	public $sessionName = 'SessProtToken'; //session name
-	public $token; //token
-	public function createToken($name=null) : string{ //create token
-		core::setError(); //reset error
-		$name = $name??$this->sessionName; //session name
-		$this->token = core::$library->crypt->hash(core::$library->string->generateString(25), 'md5'); //create token
-		$_SESSION[$name] = $this->token; //add token to session
-		return $this->token; //return token
+return $this->security = new class(){ 
+	public $version = '1.0a'; 
+	public $sessionName = 'SessProtToken'; 
+	public $token; 
+	public function createToken($name=null) : string{ 
+		core::setError(); 
+		$name = $name??$this->sessionName; 
+		$this->token = core::$library->crypt->hash(core::$library->string->generateString(25), 'md5'); 
+		$_SESSION[$name] = $this->token; 
+		return $this->token; 
 	}
-	public function checkToken(string $token, $name=null) : bool{ //check token
-		core::setError(); //reset error
-		$name = $name??$this->sessionName; //session name
-		if(!isset($_SESSION[$name])) //if not exists
-			return core::setError(1, 'No find session token'); //return error 1
-		if($token == $_SESSION[$name]) //check token
-			return true; //return true
-		return false; //return false
+	public function checkToken(string $token, $name=null) : bool{ 
+		core::setError(); 
+		$name = $name??$this->sessionName; 
+		if(!isset($_SESSION[$name])) 
+			return core::setError(1, 'No find session token'); 
+		if($token == $_SESSION[$name]) 
+			return true; 
+		return false; 
 	}
-	public function isXMLHttpRequest() : bool{ //check XMLHttpRequest
-		return (core::$library->network->getHeader('X_REQUESTED_WITH') == 'XMLHttpRequest'); //return data
+	public function isXMLHttpRequest() : bool{ 
+		core::setError(); 
+		return (core::$library->network->getHeader('X_REQUESTED_WITH') == 'XMLHttpRequest'); 
 	}
 };
 ?>
