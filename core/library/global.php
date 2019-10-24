fe9cb362bb121db59c184607a1ecb196cb1c729f
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2019-10-24 15:39:19 +0200

    1.1
    clear cache

--- core/library/global.php
+++ core/library/global.php
@@ -1,25 +1,25 @@
 <?php
-return $this->global = new class(){ //create library
-	public $version = '1.1'; //version
-	private $__globalVar = []; //var data (private)
-	public $__globalList = []; //var list
-	public function read(string $name){ //read var
-		core::setError(); //clear error
-		if(isset($this->__globalVar[$name])) //search
-			return $this->__globalVar[$name]; //return
+return $this->global = new class(){ 
+	public $version = '1.1'; 
+	private $__globalVar = []; 
+	public $__globalList = []; 
+	public function read(string $name){ 
+		core::setError(); 
+		if(isset($this->__globalVar[$name])) 
+			return $this->__globalVar[$name]; 
 		else
-			return null; //not found
+			return null; 
 	}
-	public function write(string $name, $data) : void{ //write var
-		core::setError(); //clear error
-		$this->__globalVar[$name] = $data; //set var
-		array_push($this->__globalList, $name); //add to list
+	public function write(string $name, $data) : void{ 
+		core::setError(); 
+		$this->__globalVar[$name] = $data; 
+		array_push($this->__globalList, $name); 
 		return;
 	}
-	public function unset(string $name) : void{ //delete var
-		core::setError(); //clear error
-		unset($this->__globalVar[$name]); //delete
-		$this->__globalList = array_diff($this->__globalList, [$name]); //delete from array
+	public function unset(string $name) : void{ 
+		core::setError(); 
+		unset($this->__globalVar[$name]); 
+		$this->__globalList = array_diff($this->__globalList, [$name]); 
 		return;
 	}
 }
--- core/library/string.php
+++ core/library/string.php
@@ -1,55 +1,54 @@
 <?php
-return $this->string = new class(){ //create class
-	public $version = '1.1'; //version
-	public function between(string $string, string $start, string $end, int $offset=0) : string{ //get string between
-		core::setError(); //reset error
-		$strpos1 = core::$library->string->strpos($string, '"', 0+(2*$offset))+1; //get first strpos
-		$strpos2 = core::$library->string->strpos($string, '"', 1+(2*$offset)); //get second strpos
-		return substr($string, $strpos1, $strpos2-$strpos1); //return substr
+return $this->string = new class(){ 
+	public $version = '1.1'; 
+	public function between(string $string, string $start, string $end, int $offset=0) : string{ 
+		core::setError(); 
+		$strpos1 = core::$library->string->strpos($string, '"', 0+(2*$offset))+1; 
+		$strpos2 = core::$library->string->strpos($string, '"', 1+(2*$offset)); 
+		return substr($string, $strpos1, $strpos2-$strpos1); 
 	}
-	//TODO: dodać do specyfikacji
 	public function strpos(string $string, string $searchString, int $offset = 0) : int{
-		core::setError(); //reset error
-		if($offset < 0) //check offset
-			return core::setError(1, 'integer error', 'offset must be greater than -1'); //return error
-		$stringLen = strlen($string); //get string length
-		$searchStringLen = strlen($searchString); //get search string length
-		for($i=0; $i<=$stringLen-1; $i++){ //string loop
-			if($string[$i] == $searchString[0]){ //if char == first search char
-				if($i+$searchStringLen > $stringLen) //if generate length > string length
-					continue; //continue
-				$generateString = ''; //define generate string
-				for($x=0; $x<=$searchStringLen-1; $x++) //generate loop
-					$generateString .= $string[$i+$x]; //add char
-				if($generateString == $searchString){ //if search
-					if($offset == 0) //if search
-						return $i; //return char number
-					else //search next time
-						$offset--; //offset -1
+		core::setError(); 
+		if($offset < 0) 
+			return core::setError(1, 'integer error', 'offset must be greater than -1'); 
+		$stringLen = strlen($string); 
+		$searchStringLen = strlen($searchString); 
+		for($i=0; $i<=$stringLen-1; $i++){ 
+			if($string[$i] == $searchString[0]){ 
+				if($i+$searchStringLen > $stringLen) 
+					continue; 
+				$generateString = ''; 
+				for($x=0; $x<=$searchStringLen-1; $x++) 
+					$generateString .= $string[$i+$x]; 
+				if($generateString == $searchString){ 
+					if($offset == 0) 
+						return $i; 
+					else 
+						$offset--; 
 				}
 			}
 		}
-		return -1; //return -1 if error
+		return -1; 
 	}
-	public function generateString(int $length = 15, array $data = [true, true, true, true]) : string{ //generate string
-		core::setError(); //reset error
-		$return = ''; //return value
-		$string = ''; //string value
-		if($data[0] === true) $string .= '0123456789'; //int
-		if($data[1] === true) $string .= 'abcdefghijklmnopqrstuvwxyz'; //str
-		if($data[2] === true) $string .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; //big str
-		if($data[3] === true) $string .= '!@#%^&*()_+=-}{[]?'; //add
-		$stringLen = strlen($string); //count string len
-		for($i=1; $i<=$length; $i++) //loop
-			$return .= $string[rand(1, $stringLen)-1]; //add char
-		return $return; //clean generate string
+	public function generateString(int $length = 15, array $data = [true, true, true, true]) : string{ 
+		core::setError(); 
+		$return = ''; 
+		$string = ''; 
+		if($data[0] === true) $string .= '0123456789'; 
+		if($data[1] === true) $string .= 'abcdefghijklmnopqrstuvwxyz'; 
+		if($data[2] === true) $string .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+		if($data[3] === true) $string .= '!@#%^&*()_+=-}{[]?'; 
+		$stringLen = strlen($string); 
+		for($i=1; $i<=$length; $i++) 
+			$return .= $string[rand(1, $stringLen)-1]; 
+		return $return; 
 	}
-	public function clean(string $string) : string{ //clean string
-		core::setError(); //reset error
-		$string = strip_tags($string); //strip tags
+	public function clean(string $string) : string{ 
+		core::setError(); 
+		$string = strip_tags($string); 
 		if(!get_magic_quotes_gpc())
-			$string = addslashes($string); //add slasher
-		return $string; //return clean text
+			$string = addslashes($string); 
+		return $string; 
 	}
 };
 ?>
