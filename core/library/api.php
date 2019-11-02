f4ac7c253df95032acf8ae5f4242a85e0e618f15
Krzysztof Żyłka <41385342+krzysztofzylka@users.noreply.github.com>
2019-11-02 18:57:50 +0100

    1.1
    - Zoptymalizowano kod
    - Dodano czyszczenie błędu

--- core/library/api.php
+++ core/library/api.php
@@ -1,9 +1,9 @@
 <?php
 return $this->api = new class(){
-	public $version = '1.0';
+	public $version = '1.1';
 	public function start(string $name){
-		$api_pathDir = core::$path['core'].'library/api/';
-		$api_path = $api_pathDir.basename($name).'.php';
+		core::setError();
+		$api_path = core::$path['library'].'/api/'.basename($name).'.php';
 		if(!file_exists($api_path))
 			return core::setError(1, 'api file not exists');
 		return include($api_path);
