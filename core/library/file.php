d6bd7bc5c2d146dd390cd969243693cc0f9c1af4
Krzysztof Żyłka <41385342+krzysztofzylka@users.noreply.github.com>
2020-01-03 07:30:11 +0100

    1.1
    - Dodano obsługę błędu dla funkcji fileCount
    - Dodano funkcję uploadFile

--- core/library/file.php
+++ core/library/file.php
@@ -1,10 +1,52 @@
 <?php
 return $this->file = new class(){ 
-	public $version = "1.0"; 
+	public $version = '1.1'; 
 	public function fileCount(string $path){ 
 		core::setError();
+		if(!file_exists($path))
+			return core::setError(1, 'path not exists');
 		$tmp = new FilesystemIterator($path, FilesystemIterator::SKIP_DOTS);
 		return iterator_count($tmp);
+	}
+	public function uploadFile(string $fileFormName, string $newPath, string $newName = null, array $option = []) : bool{
+		core::setError();
+		if(!is_array($option))
+			$option = [];
+		if(!isset($option['ignoreFileExists']))
+			$option['ignoreFileExists'] = false;
+		if(!isset($option['maxFileSize']))
+			$option['maxFileSize'] = -1;
+		if(!isset($option['fileExtension']))
+			$option['fileExtension'] = null;
+		if(!isset($_FILES[$fileFormName]))
+			return core::setError(1, 'fileFormName not exists');
+		$file = $_FILES[$fileFormName];
+		if($file['error'] > 0)
+			return core::setError(2, 'upload file error', 'error: '.$file['error']);
+		if($option['fileExtension'] <> null){
+			$ext = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+			$option['fileExtension'] = strtolower($option['fileExtension']);
+			if(array_search($ext, explode(',', $option['fileExtension'])) === false)
+				return core::setError(7, 'invalid file extension', 'possible extensions: '.$option['fileExtension']);
+		}
+		if($option['maxFileSize'] > -1)
+			if($file['size'] >= $option['maxFileSize'])
+				return core::setError(6, 'file size is too large', 'file size: '.core::$library->memory->formatBytes($file['size']).', max file size: '.core::$library->memory->formatBytes($option['maxFileSize']));
+		$newName = $newName??$file['name'];
+		if(!file_exists($file['tmp_name']))
+			return core::setError(3, 'temp file not exists');
+		$newPathLastChr = substr($newPath, strlen($newPath)-1);
+		if($newPathLastChr <> '\\' and $newPathLastChr <> '/')
+			$newPath .= '\\';
+		$newPath .= $newName;
+		if(file_exists($newPath))
+			if($option['ignoreFileExists'])
+				unlink($newPath);
+			else
+				return core::setError(4, 'file is already exists', $newPath);
+		if(!copy($file['tmp_name'], $newPath))
+			return core::setError(5, 'error copy file');
+		return true;
 	}
 }
 ?>
