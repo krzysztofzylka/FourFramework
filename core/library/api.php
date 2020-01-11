ef04c94b04828e0e8b27b7378b97cf53e87afd7e
Krzysztof Żyłka <41385342+krzysztofzylka@users.noreply.github.com>
2020-01-11 09:38:56 +0100

    1.2
    Dodano zmienną z listą załadowanych API

--- core/library/api.php
+++ core/library/api.php
@@ -1,11 +1,13 @@
 <?php
 return $this->api = new class(){
-	public $version = '1.1';
+	public $version = '1.2';
+	public $list = [];
 	public function start(string $name){
 		core::setError();
 		$api_path = core::$path['library'].'/api/'.basename($name).'.php';
 		if(!file_exists($api_path))
 			return core::setError(1, 'api file not exists');
+		array_push($this->list, $name);
 		return include($api_path);
 	}
 }; 
