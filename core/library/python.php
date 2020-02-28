063fb5f10ceb060a86208626d7c616bb13178b68
Krzysztof Żyłka <41385342+krzysztofzylka@users.noreply.github.com>
2020-02-28 09:40:04 +0100

    1.1
    Dodano obsługę błędu sprawdzającego czy plik istnieje w funkcji run

--- core/library/python.php
+++ core/library/python.php
@@ -1,8 +1,10 @@
 <?php
 return $this->python = new class(){ 
-	public $version = '1.0'; 
+	public $version = '1.1'; 
 	public function run(string $path){
 		core::setError();
+		if(!file_exists($path))
+			return core::setError(1, 'file not found');
 		$command = escapeshellcmd($path);
 		$output = shell_exec($command);
 		return $output;
