bed2148f08eae6e96845a67bc8fab6fd440afc5c
Krzysztof Żyłka <41385342+krzysztofzylka@users.noreply.github.com>
2020-03-15 06:22:38 +0100

    1.2a
    Dostosowano do nowej wersji rdzenia

--- core/library/api.php
+++ core/library/api.php
@@ -1,10 +1,10 @@
 <?php
 return $this->api = new class(){
-	public $version = '1.2';
+	public $version = '1.2a';
 	public $list = [];
 	public function start(string $name){
 		core::setError();
-		$api_path = core::$path['library'].'/api/'.basename($name).'.php';
+		$api_path = core::$path['library_api'].'/'.basename($name).'.php';
 		if(!file_exists($api_path))
 			return core::setError(1, 'api file not exists');
 		array_push($this->list, $name);
