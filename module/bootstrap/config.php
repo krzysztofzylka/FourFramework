6d399c0e6d49ba2be3f07fc7b038d1fcf4c0982a
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-02-22 13:44:08 +0100

    Wersja 1.1
    Wgrano wersję bootstrap 1.1, od teraz moduł ściąga sobie potrzebne biblioteki z sieci

--- module/bootstrap/config.php
+++ module/bootstrap/config.php
@@ -2,5 +2,7 @@
 return [
 	'include' => [],
 	'moduleFile' => 'main.php',
+	'version' => '1.1',
+	'uniqueID' => '984098a692bb65199be0d4cf6f1f1ebc'
 ];
 ?>
--- module/bootstrap/main.php
+++ module/bootstrap/main.php
@@ -1,5 +1,4 @@
 <?php
-// echo "asdasfas";
 return new class(){ //create class
 	public function __construct(){ //construct
 		$config = core::$module_add['bootstrap']['config']; //load config to variable
@@ -8,9 +7,9 @@ return new class(){ //create class
 		if($search === false) //if smarty not exists
 			return core::setError(1, 'smarty not exists'); //return error 1
 		$smarty = core::$module['smarty']->smarty; //get smarty
-		$bootstrap = '<link rel="stylesheet" href="'.$config['path'].'bootstrap/bootstrap.min.css">
-		<script src="'.$config['path'].'bootstrap/jquery.min.js"></script>
-		<script src="'.$config['path'].'bootstrap/bootstrap.min.js"></script>';
+		$bootstrap = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>';
 		$smarty->assign('bootstrap', $bootstrap); //assign bootstrap
 	}
 }
