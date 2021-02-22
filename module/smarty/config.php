1917032859340dc6b464d35c75342e699e8e23af
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-02-22 13:43:15 +0100

    Wersja 1.1
    Wgrano najnowszą wersję biblioteki smarty

--- module/smarty/config.php
+++ module/smarty/config.php
@@ -2,5 +2,7 @@
 return [
 	'include' => [],
 	'moduleFile' => 'main.php',
+	'version' => '1.1',
+	'uniqueID' => 'ebc3784a76b304a6f58d5ab167df00ef'
 ];
 ?>
--- module/smarty/main.php
+++ module/smarty/main.php
@@ -10,12 +10,12 @@ return new class(){ //create class
 		$this->smarty->cache_lifetime = 5; //cache lifetime
 		$temp = core::$path['temp'].'smarty/'; //temp dir
 		$path = ['cache' => $temp.'cache/','compile' => $temp.'templates_c/']; //path list
-		$this->templateDir = core::$info['reversion'].'template/'; //set templateDir
+		$this->templateDir = core::$info['frameworkPath'].'template/'; //set templateDir
 		if(!file_exists($this->templateDir)) //if not exists
 			mkdir($this->templateDir, 0777, true); //create template dir
 		$protectedFile = $this->templateDir.'.htaccess'; //htaccess path
 		if(!file_exists($protectedFile)) //if not exists
-			file_put_contents($protectedFile, 'deny from all', FILE_APPEND); //write to file
+			 copy($config['path'].'.htaccess', $protectedFile); //copy .htaccess file
 		foreach($path as $dirPath) //path loop
 			if(!file_exists($dirPath)) //if not file exists
 				mkdir($dirPath, 0777, true); //create dir
@@ -25,6 +25,14 @@ return new class(){ //create class
 	}
 	public function setTemplateDir(string $path){ //set template Dir
 		$this->smarty->setTemplateDir($path); //set template dir in smarty
+	}
+	public function setCaching($data=5){
+		if($data === false){
+			$this->smarty->caching = false;
+			return true;
+		}
+		$this->smarty->cache_lifetime = (int)$data;
+			return true;
 	}
 }
 ?>
