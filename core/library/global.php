3e595280b4a432ad98f8dbed8ff5cf8277626b9f
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-03-31 06:34:43 +0200

    1.3
    - Poprawiono błąd związany z duplikowaniem danych w tablicy __globalList
    
    1.2
    - Dodano funkcje createArray oraz writeArray

--- core/library/global.php
+++ core/library/global.php
@@ -1,6 +1,6 @@
 <?php
 return $this->global = new class(){ 
-	public $version = '1.1a'; 
+	public $version = '1.3'; 
 	public $__globalVar = []; 
 	public $__globalList = []; 
 	public function read(string $name){ 
@@ -12,7 +12,7 @@ return $this->global = new class(){
 	public function write(string $name, $data) : void{ 
 		core::setError(); 
 		$this->__globalVar[$name] = $data; 
-		array_push($this->__globalList, $name); 
+		$this->_addDataToGlobalList($name);
 		return;
 	}
 	public function unset(string $name) : void{ 
@@ -20,6 +20,25 @@ return $this->global = new class(){
 		unset($this->__globalVar[$name]); 
 		$this->__globalList = array_diff($this->__globalList, [$name]); 
 		return;
+	}
+	public function createArray(string $name) : void{
+		core::setError();
+		$this->__globalVar[$name] = []; 
+		$this->_addDataToGlobalList($name);
+		return;
+	}
+	public function writeArray(string $name, string $arrayName, $data) : void{
+		core::setError(); 
+		if($arrayName == '')
+		$this->__globalVar[$name][] = $data; 
+		else
+			$this->__globalVar[$name][$arrayName] = $data; 
+		$this->_addDataToGlobalList($name);
+		return;
+	}
+	private function _addDataToGlobalList($name){
+		if(is_bool(array_search($name, $this->__globalList)) == true)
+			array_push($this->__globalList, $name); 
 	}
 }
 ?>
