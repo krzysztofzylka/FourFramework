828f004343983352b2a37264c43ad976adbbeb4d
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-04-20 19:57:41 +0200

    Zmodyfikowano klasę view oraz dodano obsługę pliku konfiguracyjnego

---
+++ core/configuration/view.php
@@ -0,0 +1,13 @@
+<?php
+// return [
+    // 'viewType' => [
+        // 'prepend' => [
+            // 'blankPage' => '<div><h1>{$title|Tytuł strony}</h1>',
+        // ],
+        // 'append' => [
+            // 'blankPage' => '</div>',
+        // ]
+    // ],
+    // 'GuiHelperModel' => 'GuiHelper'
+// ]
+?>
--- core/view.php
+++ core/view.php
@@ -1,19 +1,90 @@
 <?php
 class view{
-    protected $__Class__viewPath = null;
-    protected $__Class__viewData = [];
-    public function __construct($viewPath, $viewData=[]){
-        $this->__Class__viewPath = $viewPath;
-        $this->__Class__viewData = $viewData;
+    private $__viewTypeList = [
+        'prepend' => [],
+        'append' => []
+    ];
+    private $__viewGuiHelper = null;
+    private $__viewType = null;
+    private $__viewOption = [];
+    private $__viewPath;
+    private $__viewVariable = [];
+    private $__controllerObject = null;
+
+    public function __construct($__viewPath, $__viewOption=[]){
+        core::setError();
+        $this->__viewPath = $__viewPath;
+        $this->__viewOption = $__viewOption;
+        $this->_loadConfiguration();
+
+		if (isset($__viewOption['controllerObject'])
+			&& is_object($__viewOption['controllerObject'])
+		) {
+			$this->__controllerObject = $__viewOption['controllerObject'];
+			$this->__viewVariable =  $__viewOption['controllerObject']->__view['variable'];
+			$this->__viewType =  $__viewOption['controllerObject']->__view['viewType']??null;
+		}
+
+		foreach ($this->__viewVariable as $__variableName => $__variableValue) {
+			${$__variableName} = $__variableValue;
+		}
+		if (!is_null($this->__viewGuiHelper)) {
+			$this->{$this->__viewGuiHelper} = core::loadModel(
+				$this->__viewGuiHelper,
+				[
+					'returnOnly' => true
+				]
+			);
+		}
+
+		ob_start();
+		echo $this->_loadViewType('prepend');
+		include($this->__viewPath);
+		echo $this->_loadViewType('append');
+		$__viewData = ob_get_contents();
+		ob_get_clean();
+		echo $__viewData;
     }
-    public function bind($name, $value){
-        $this->__Class__viewData[$name] = $value;
-    }
-    public function execute(){
-        foreach ($this->__Class__viewData as $name => $value) {
-            ${$name} = $value;
+    private function _loadViewType($type){
+		if($this->__viewType <> null and isset($this->__viewTypeList[$type][$this->__viewType])){
+			$string = $this->__viewTypeList[$type][$this->__viewType];
+			$string = preg_replace_callback('/{\$([a-zA-Z_\-.0-9]*)([|])?(.*?)}/sm', function ($matches) {
+				switch (true) {
+					case isset($this->__viewVariable[$matches[1]]):
+						return $this->__viewVariable[$matches[1]];
+						break;
+					case $matches[2] === '|':
+						return $matches[3];
+						break;
+					default:
+						switch ($matches[1]) {
+							case '__randomGuiId':
+								$this->__viewVariable[$matches[1]] = md5(uniqid(rand(), true));
+								return $this->__viewVariable[$matches[1]];
+								break;
+						}
+
+						return $matches[0];
+						break;
+				}
+			}, $string);
+			return $string;
+		}
+		return '';
+	}
+    private function _loadConfiguration(){
+        $configurationPath = core::$path['configuration'].'view.php';
+        if (file_exists($configurationPath)) {
+            $configuration =  include($configurationPath);
+
+            if (isset($configuration['viewType'])) {
+                $this->__viewTypeList = $configuration['viewType'];
+            }
+
+            if (isset($configuration['GuiHelperModel'])) {
+                $this->__viewGuiHelper = $configuration['GuiHelperModel'];
+            }
         }
-        return include($this->__Class__viewPath);
     }
 }
 ?>
