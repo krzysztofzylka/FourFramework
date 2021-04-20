17a21f61494b80c1576c13bc383dbe6870743b46
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-04-20 19:58:06 +0200

    Poprawiono klasę kontrolera
    dodano funkcje obsługujące widok

--- core/controller.php
+++ core/controller.php
@@ -1,25 +1,47 @@
 <?php
 class core_controller{
-    public function loadModel(string $modelName){
-        $this->{$modelName} = core::loadModel(
+    public $__view = [
+        'variable' => [],
+        'viewType' => null,
+    ];
+
+    public function loadModel(string $modelName) {
+        core::setError();
+
+        $this->{str_replace('.', '_', $modelName)} = core::loadModel(
             $modelName,
             [
                 'returnOnly' => true
             ]
         );
     }
-    public function loadView(string $viewName){
+    public function loadView(string $viewName) {
+        core::setError();
+
         $viewPath = core::loadView(
             $viewName,
             [
                 'returnPathOnly' => true
             ]
         );
-        return new view($viewPath);
+
+        return new view(
+            $viewPath,
+            [
+                'controllerObject' => $this
+            ]
+        );
+    }
+
+    public function viewSetVariable(string $variableName, $variableValue) {
+        $this->__view['variable'][$variableName] = $variableValue;
+    }
+    public function viewSetType(string $typeName) {
+        $this->__view['viewType'] = $typeName;
     }
 }
 
-return new class(){
+return new class() {
     public $_lastLoadController;
     public $_list = [];
 }
