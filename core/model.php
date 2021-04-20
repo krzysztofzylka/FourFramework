011db8bfa5b2e8ce42ee8f37a64e0c76d872725e
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2021-04-20 19:57:03 +0200

    Dodano klasę rozszerzającą model

--- core/model.php
+++ core/model.php
@@ -1,4 +1,15 @@
 <?php
+
+class core_model{
+    public function loadModel(string $modelName){
+        $this->{str_replace('.', '_', $modelName)} = core::loadModel(
+            $modelName,
+            [
+                'returnOnly' => true
+            ]
+        );
+    }
+}
 return new class(){
     public $_list = [];
 }
